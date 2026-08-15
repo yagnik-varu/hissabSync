39012922f114de5316aff559f715f6839e8d4a03
Yagnik <yagnikvaru2003@gmail.com>
2026-08-15 12:23:24 +0530

    chore: manual end-to-end walkthrough of expense-to-payout pipeline

--- src/modules/reimbursement/reimbursement.module.ts
+++ src/modules/reimbursement/reimbursement.module.ts
@@ -3,9 +3,11 @@ import { ReimbursementService } from './services/reimbursement.service';
 import { ReimbursementRepository } from './repositories/reimbursement.repository';
 import { ReimbursementController } from './controllers/reimbursement.controller';
 import { PrismaModule } from '../../database/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+import { RoomModule } from '../room/room.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, RoomModule],
   controllers: [ReimbursementController],
   providers: [ReimbursementService, ReimbursementRepository],
   exports: [ReimbursementService],
